75bb744eb252daf4fb1d4fa87e7830bb5a6e3b69
AnnaKurniaty <Annakuykuy25@gmail.com>
2023-03-12 14:44:15 +0700

    next

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
@@ -18,11 +20,11 @@ public class InventoryServiceApplication {
     public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
         return args -> {
             Inventory inventory = new Inventory();
-            inventory.setSkuCode("Cake");
+            inventory.setSkuCode("iphone_13");
             inventory.setQuantity(100);
 
             Inventory inventory1 = new Inventory();
-            inventory1.setSkuCode("Cake-Chocolate");
+            inventory1.setSkuCode("iphone_13_red");
             inventory1.setQuantity(0);
 
             inventoryRepository.save(inventory);
